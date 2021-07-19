8f6301f7cb8a903b6c96331ce39f01a913ff7e65
glenbuck503 <glenbuck503@gmail.com>
2021-07-18 18:47:59 -0700

    add form field to resuable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Words from './Words';
+import ReusableForm from './ReusableForm';
 
 function App() {
   return (
     <React.Fragment>
       < Words />
+      < ReusableForm/>
     </React.Fragment>
   );
 }
---
+++ src/ReusableForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='words'
+          placeholder='Words' />
+        <button type='submit'>{props.buttonText}</button>
+        
+
+        
+
+        
+      </form>
+      
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+
+};
+
+export default ReusableForm;
