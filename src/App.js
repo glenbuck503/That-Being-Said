c029eb664da3167e582e7d062ec3c91f2f048040
glenbuck503 <glenbuck503@gmail.com>
2021-07-19 22:54:07 -0700

    fixed hard coded translated text to show back

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import Words from './Words';
+import Words from './Words';
 import ReusableForm from './ReusableForm';
 import Header from './Header'
 import WordControl from './WordControl';
@@ -8,8 +8,9 @@ import WordControl from './WordControl';
 function App() {
   return (
     <React.Fragment>
-      < Header />
-      <div class="inputForm">
+      {/* < Header /> */}
+      <Words />
+      <div className="inputForm">
       < ReusableForm/>
       </div>
     </React.Fragment>
